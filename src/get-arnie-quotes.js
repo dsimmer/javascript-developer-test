04faebf37ea452654305e3cfb9f4ae64aa620325
Dylan Simmer <goldenoblivion@gmail.com>
2021-04-10 22:37:59 +1000

    [JIRA-XXXX] Implement getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,29 @@
 const { httpGet } = require('./mock-http-interface');
 
+const HTTPOK = 200
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = []
+
+  for (const url of urls) {
+    results.push(
+      httpGet(url)
+        .then((res) => {
+          const message = JSON.parse(res.body).message
+          if (res.status === HTTPOK) {
+            return {"Arnie Quote": message}
+          }
+          return {"FAILURE": message}
+        })
+        // Not required for this test since the mocked httpGet can't throw and returns proper JSON,
+        // but the library that replaces this in the future may not do so
+        .catch((err) => {
+          // I assume we dont mind leaking a HTTP or JSON error to the user here
+          return {"FAILURE": err.message}
+        }))
+  }
+
+  return Promise.all(results)
 };
 
 module.exports = {
