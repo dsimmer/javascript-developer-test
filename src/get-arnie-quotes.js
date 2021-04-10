4ff8f44c1b214c1bed6274f3d844aaca56e876e6
Dylan Simmer <goldenoblivion@gmail.com>
2021-04-10 23:14:17 +1000

    [JIRA-XXXX] Streamline getArnieQuotes, function is simple enough to condense

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,30 +1,24 @@
 const { httpGet } = require('./mock-http-interface');
 
-const HTTPOK = 200
+const HTTPOK = 200;
 
-const getArnieQuotes = async (urls) => {
-  const results = []
-
-  for (const url of urls) {
-    results.push(
+const getArnieQuotes = async (urls) =>
+  Promise.all(
+    urls.map((url) =>
       httpGet(url)
         .then((res) => {
-          const message = JSON.parse(res.body).message
+          const message = JSON.parse(res.body).message;
           if (res.status === HTTPOK) {
-            return {"Arnie Quote": message}
+            return {"Arnie Quote": message};
           }
-          return {"FAILURE": message}
+          return {"FAILURE": message};
         })
         // Not required for this test since the mocked httpGet can't throw and returns proper JSON,
         // but the library that replaces this in the future may not do so
         .catch((err) => {
           // I assume we dont mind leaking a HTTP or JSON error to the user here
-          return {"FAILURE": err.message}
-        }))
-  }
-
-  return Promise.all(results)
-};
+          return {"FAILURE": err.message};
+        })));
 
 module.exports = {
   getArnieQuotes,
